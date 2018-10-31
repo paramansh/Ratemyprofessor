afba4ff7f379217b86b75456f56adbc3a8b67260
paramansh <paramansh9895@gmail.com>
2018-10-31 16:27:08 +0530

    Prof Schema

--- models/Prof.js
+++ models/Prof.js
@@ -4,9 +4,23 @@ const mongoose = require('mongoose');
 
 
 const profSchema = mongoose.Schema({
-    name: String,
-    age: Number,
-    nationality: String
+    profile: {
+        name: String,
+        email: String,
+        Department: String,
+        homepage: String,
+        age: Number,
+        nationality: String
+    },
+    rating: {
+        param1: Float,
+        param2: Float,
+        param3: Float,
+        param4: Float,
+    }
+    comments: {
+        c: String,
+    }
  });
 
 const Professor = mongoose.model("Professor", profSchema);
