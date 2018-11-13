2c9c1b31aa740ad1b817209c3602f8b75e572d34
paramansh <paramansh9895@gmail.com>
2018-11-13 17:26:26 +0530

    icon

--- controllers/stats.js
+++ controllers/stats.js
@@ -1,5 +1,21 @@
+const Professor = require('../models/Prof');
+
 exports.index = (req, res) => {
-    res.render('stats', {
-      title: 'Statistics'
-    });
-  };
+  // Professor
+  //   .find()
+  //   .max('rating.numRatings')
+  //   .limit(3)
+  //   .exec(function (err, member) {
+  //     console.log(member);
+  //   })
+
+  // Professor
+  //   .find()
+  //   .exec(function (err, member) {
+  //     console.log(member);
+  //   })
+
+  res.render('stats', {
+    title: 'Statistics'
+  });
+};
